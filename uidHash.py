bead849c72ad3a738ba72cb963393e6c67b8e38a
Sam Kome <sam_kome@cuc.claremont.edu>
2017-09-18 15:33:49 -0700

    better error management to handle WMS patron data

--- uidHash.py
+++ uidHash.py
@@ -10,63 +10,65 @@ from hashlib import sha256
 doc=""" 
 %prog [input file] [output file] 
 input file contains the user ids to be hashed. User ID takes form 'name@domain'
-Outputfile will include everthing in the input file but uids will have been securely hashed
+Outputfile will include everything in the input file but uids will have been securely hashed
 """
-	
-config = ConfigParser.RawConfigParser()
-config.read('uidHash.cfg')
-UNAMESALT = config.get('Auth', 'salt')
-
-def getUIDStartPos(logLine):
-    doc="""
-    return the uid and its start and end positions 
-    """
-    match = re.search(r'[\w.-]+@[\w.-]+', logLine)
-    if match:
-        return match.group(), match.span()
-def getCampus(uid):
-    campus = uid.split('@')[1].split('.')[0]
-    return campus
-    
+DELIM = '\t'
+PTYPE_POS = 2
 def hash_uid(uname, salt=None):
     if salt is None:
         salt = uuid.uuid4().hex
-    hashed_uid = sha256(uname + salt).hexdigest()
+    hashed_uid = sha256(uname.lower() + salt).hexdigest()
     return (hashed_uid)
 
 def verify_password(uid, hashed_uid, salt):
     re_hashed, salt = hash_uid(uid, salt)
     return re_hashed == hashed_uid
+	
+def getUIDPos(logLine, reString):
+    doc="""
+    return the uid and its start and end positions 
+    """
+    match = re.search(reString, logLine.lower().strip())
+    if match:
+        return match.group(), match.span()
 
+def getCampus(uid):
+    campus = uid.split('@')[1].split('.')[0].lower().strip()
+    return campus
+
+def getpType(logLine):
+    pType=logLine.split(DELIM)[PTYPE_POS].strip('"\r\n').lower()
+    return pType.strip()
+     
 def getSession(lLogLine):
     return lLogLine.split(' ')[0]
 
 def getIPSubnet(lLogLine):
     match = re.search('([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})', lLogLine)
-    return match.group()
+    if match:
+        return match.group()
 
+config = ConfigParser.RawConfigParser()
+config.read('uidHash.cfg')
+UNAMESALT = config.get('Auth', 'salt')
 
 if __name__ == '__main__':
     uidFile=sys.argv[1] 
     outputfile = sys.argv[2]
     with open(uidFile,'r') as uidsf, open(outputfile, 'w') as hasheduidsf:
-        csvf = csv.writer(hasheduidsf,delimiter='\t')
-        #find the uids start position (per line)
+        csvf = csv.writer(hasheduidsf,delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        csvf.writerow(['uuid','campus','patronType'])
         for line in uidsf:
-            try:
-                uidInfo = getUIDStartPos(line)                
-                uid = uidInfo[0]
-                huid = hash_uid(uid,UNAMESALT)
-                campus = getCampus(uid)
-                uidPos = uidInfo[1]
-                preamble = line[0:uidPos[0]].strip('"')
-                remainder = line[uidPos[1]:].strip().split('\t')[1].strip('"')#for WMS report
-                #remainder = line[uidPos[1]:].strip()#for ezp
-                #print ("preamble: {}\nuid:{}\nhuid:{}\ncampus:{}\nremainder: {}").format(preamble, uid,huid, campus, remainder)
-                #csvf.writerow([huid,campus,preamble,getSession(remainder), getIPSubnet(remainder)])#for ezp report data
-                csvf.writerow([huid,campus,remainder])#for WMS
-            except TypeError:
-                print "Error value: {}".format(line)
-        #find the end of the uid (per line)
-        #create a new line with the pre-uid text, hashed uid, post-uid text
-        #write that to the new file.
+            if line.find('N/A')==-1:
+                uidInfo = getUIDPos(line, r'[\w.-]+@[\w.-]+')                
+                try:
+                    uid = uidInfo[0]
+                    huid = hash_uid(uid.split('@')[0],UNAMESALT)
+                    campus = getCampus(uid)
+                    print('Parsed: {},{}').format(uid,campus)
+                except:
+                    print("Parsing error uid, campus values: {}").format(line)
+                else:
+                    remainder = getpType(line)
+                    print('{}, {}, {}').format(huid, campus, remainder)
+                    csvf.writerow([huid,campus,remainder])
